ed4a901975624ebc1f7fd0720f4e5a3704505c05
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-10 01:47:51 +0300

    add bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "channels"
+import "bootstrap"
+import "../stylesheets/application"
+
+import * as ActiveStorage from "@rails/activestorage"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
