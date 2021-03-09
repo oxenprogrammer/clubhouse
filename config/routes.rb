a45539076e2a3a8c2cda13b69abb514ba812f95d
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-09 16:15:33 +0300

    feat(view): update root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   get 'static_pages/home'
-  root 'static_pages#home'
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
