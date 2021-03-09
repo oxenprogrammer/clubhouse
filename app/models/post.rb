5c84f80241890c1e38c70e60854347614b1218e2
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-10 00:51:42 +0300

    fixup! chore(rules): run rubocop -A

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, uniqueness: true
+  validates :content, presence: true, length: { minimum: 50, maximum: 500 }
 end
--- db/migrate/20210309094943_devise_create_users.rb
+++ db/migrate/20210309094943_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
