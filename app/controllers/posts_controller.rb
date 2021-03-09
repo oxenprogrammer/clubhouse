3e4d3ffef644bef0e0cee1ff10eaabde82cea586
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-09 23:00:02 +0300

    feat(view): add display username

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
-    @post = current_user.posts.build
   end
 
   # GET /posts/1 or /posts/1.json
