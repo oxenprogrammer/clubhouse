761a75e8e72f1a787ffac4e127d0e6fe078f665e
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-10 19:17:36 +0300

    feat(controller): update post controller order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
   # GET /posts/1 or /posts/1.json
