e796e7bfbb072c08db4396f3b84d74878eb8e4e9
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-10 16:20:58 +0300

    hotfix(route): fix logout bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :posts, only: %i[new create index show]
   resources :users, only: %i[new create index]
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'static_pages/home'
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
